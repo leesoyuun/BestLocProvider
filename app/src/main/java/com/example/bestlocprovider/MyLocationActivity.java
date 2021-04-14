eb2bec92b4a528e717f00f8f00c162d0d989c688
leesoyun <leesoyun4206@naver.com>
2021-04-14 14:23:30 +0900

    complete onResume_H.W(re)

--- app/src/main/java/com/example/bestlocprovider/MyLocationActivity.java
+++ app/src/main/java/com/example/bestlocprovider/MyLocationActivity.java
@@ -70,7 +70,6 @@ public class MyLocationActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        super.onResume();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
